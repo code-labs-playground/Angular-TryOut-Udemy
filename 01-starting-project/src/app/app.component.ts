ee9172ef9990c85d192382cc4b3e16048158e781
Lahiru Liyanage <lhlahiru95@gmail.com>
2025-06-23 12:21:14 +0530

    Converted the whole app to module base version

--- 01-starting-project/src/app/app.component.ts
+++ 01-starting-project/src/app/app.component.ts
@@ -1,19 +1,20 @@
 import { Component, Input } from '@angular/core';
 import { NgFor, NgIf } from '@angular/common';
-import { HeaderComponent } from "./header/header.component";
-import { UserComponent } from './user/user.component';
+// import { HeaderComponent } from "./header/header.component";
+// import { UserComponent } from './user/user.component';
+// import { TasksComponent } from './tasks/tasks.component';
 import { DUMMY_USERS } from './dummy-users';
-import { TasksComponent } from './tasks/tasks.component';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [HeaderComponent, UserComponent, TasksComponent],
+  standalone: false,
+  // imports: [HeaderComponent, UserComponent, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  users = DUMMY_USERS
+  users = DUMMY_USERS;
   selectedUserId?: string;
 
   get selectedUser() {
