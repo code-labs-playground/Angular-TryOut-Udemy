9a90f67fdaa215c6da461d012dc780a220fe225e
Lahiru Liyanage <lhlahiru95@gmail.com>
2025-06-23 15:29:45 +0530

    Learning how to work with Complex Module-based App

--- 01-starting-project/src/app/app.module.ts
+++ 01-starting-project/src/app/app.module.ts
@@ -1,26 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+// import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { TasksComponent } from './tasks/tasks.component';
+// import { TasksComponent } from './tasks/tasks.component';
 import { UserComponent } from './user/user.component';
 import { CardComponent } from './shared/card/card.component';
-import { NewTaskComponent } from './tasks/new-task/new-task.component';
-import { TaskComponent } from './tasks/task/task.component';
+// import { NewTaskComponent } from './tasks/new-task/new-task.component';
+// import { TaskComponent } from './tasks/task/task.component';
 import { SharedModule } from './shared/shared.module';
+import { TasksModule } from './tasks/tasks.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     UserComponent,
-    TasksComponent,
-    NewTaskComponent,
-    TaskComponent,
   ],
-  imports: [BrowserModule, FormsModule, SharedModule],
+  imports: [BrowserModule, SharedModule, TasksModule],
   providers: [],
   bootstrap: [AppComponent],
 })
